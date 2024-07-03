0f53bf778ad3d2d8aa4ec6c62609ced56310b626
KISHALAY BHATTACHARYA <64585126+kishalayb18@users.noreply.github.com>
2024-07-03 22:53:22 +0530

    Feature/kubernetes: Changes on Kubernetes, Helm Manifests (#14)
    * restructured helm common folders
    
    * ingress files
    
    * new folder structure for book info manifests
    
    * service mesh
    
    * networking manifests files
    
    new project folder

---
+++ projects/py-flask/application/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///names.db'
+db = SQLAlchemy(app)
+
+class Name(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        if name:
+            new_name = Name(name=name)
+            db.session.add(new_name)
+            db.session.commit()
+            return redirect('/')
+    names = Name.query.all()
+    return render_template('index.html', names=names)
+    # return render_template('index.html')
+
+@app.route('/names', methods=['GET'])
+def names():
+    names = Name.query.all()
+    return render_template('namelist.html', names=names)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000)
